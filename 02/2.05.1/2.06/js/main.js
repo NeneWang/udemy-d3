4383acb6f13e73a47422288418f32f6277b44062
NelsonWang <wangnelson2@gmail.com>
2022-08-10 10:26:44 -0400

    main | 2.06

--- 02/2.05.1/2.06/js/main.js
+++ 02/2.05.1/2.06/js/main.js
@@ -4,7 +4,7 @@
 *    2.6 - Selections and data joins
 */
 
-const data = [25, 20, 10, 12, 15]
+const data = [25, 20, 10, 50]
 
 const svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
